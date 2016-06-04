810c9c5599ec6b3e8a965bd428916f6a5caf1eaa
JoelGG <j.grimmer11@bristolfreeschool.org.uk>
2016-06-04 10:49:01 +0100

    Changed it a bit

---
+++ Banner.py
@@ -0,0 +1,10 @@
+def banner(name,character):
+	name = ".::" + str(name) + "::."
+	nameLength = len(name)
+	charLength = len(character)
+	overLength = nameLength/charLength
+	print(character*overLength)
+	print(name)
+	print(character*overLength)
+
+banner("Joel Grimmer","][")
